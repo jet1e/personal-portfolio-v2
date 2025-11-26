ff599ac963ced056cfa383c52b8650bc6236c3fe
Jet Le <jet05le@gmail.com>
2025-11-26 20:48:50 +0800

    Fixed nav bar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef, useState } from 'react';
+
 const tabs = [
     { id: 'about', label: 'About' },
     { id: 'experience', label: 'Experience' },    
@@ -7,15 +9,39 @@ const tabs = [
 ];
 
 export default function Navigation({ activeTab, onTabChange }) {
+    const navRef = useRef(null) // reference to hold button elements
+
+    // Slider current state
+    const [indicatorStyle, setIndicatorStyle] = useState({
+        left: '0px',
+        width: '0px'
+    });
+
+    useEffect(() => {
+        const activeBtn = navRef.current.querySelector(`.tab-button[data-section="${activeTab}"]`);
+
+        if (activeBtn) {
+            const navRect = navRef.current.getBoundingClientRect();
+            const btnRect = activeBtn.getBoundingClientRect();
+
+            const newLeft = btnRect.left - navRect.left
+
+            setIndicatorStyle({
+                left: `${newLeft}px`,
+                width: `${btnRect.width}px`
+            });
+        }
+    }, [activeTab]);
+
     return (
-        <nav className="tabs-nav">
+        <nav className="tabs-nav" ref={navRef}>
+            <div className="tab-indicator" style={indicatorStyle}></div>
             {tabs.map((tab) => (
                 <button
                     key={tab.id}
                     data-section={tab.id}
                     className={`tab-button ${activeTab === tab.id ? 'active' : ''}`}
-                    // This is the key difference from your old JS: we call a function passed from the parent
-                    onClick={() => onTabChange(tab.id)}
+                    onClick={() => onTabChange(tab.id)} // Call function to make change
                 >
                     {tab.label}
                 </button>
