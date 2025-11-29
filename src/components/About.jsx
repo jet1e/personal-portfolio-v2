ee0e3577639a3f935af8f44a843d9f0a25470b1f
Jet Le <jet05le@gmail.com>
2025-11-29 09:28:10 +0800

    Wave animation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import TerminalCard from './TerminalCard';
 export default function About() {
     return(
         <section id="about" className="portfolio-section">
-            <h3 className="section-title"> Hi there <span className="wave">👋</span></h3>
+            <h3 className="section-title"> Hi there <span className="about-wave">👋</span></h3>
             <p className="section-text">I’m a final-year Bachelor of Advanced Science Honours student in Perth. I am fascinated by large-scale, high-impact R&D work with many industry and research experiences across a wide range of challenges. Currently, I have received a scholarship for the CSIRO’s Next Generation Graduates Program (NGGP) and am researching privacy-preserving methods for elderly fall detection.</p>
             <h3 className="section-title sub-title">Education</h3>
             <div className="edu-grid">
